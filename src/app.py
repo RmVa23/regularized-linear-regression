0ae92d7af8baa520659f485ad395c6ee3c080905
RmVa23 <148461819+RmVa23@users.noreply.github.com>
2024-03-15 19:21:21 +0000

    conexion bd e importacion

--- src/app.py
+++ src/app.py
@@ -1,4 +1,12 @@
+import pandas as pd
 from utils import db_connect
+
 engine = db_connect()
 
-# your code here
+
+engine
+if engine:
+    print("Conexión exitosa")
+
+data = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/regularized-linear-regression-project-tutorial/main/demographic_health_data.csv')
+data.to_sql('tabla health', engine, if_exists='replace', index=False)
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-import pandas as pd
+
 
 # load the .env file variables
 load_dotenv()
